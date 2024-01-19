8cc6661deb9643c71f9937740a3755c434c3f75a
Abood3dd <abood3dd4@gmail.com>
2024-01-19 15:50:48 +0300

    adding js to kasper

---
+++ Kasper/main.js
@@ -0,0 +1,37 @@
+const right_arrow = document.querySelector(".right");
+const left_arrow = document.querySelector(".left");
+const landing = document.querySelector(".landing");
+const bullet = document.querySelectorAll(".bullet");
+
+let image = parseInt(window.localStorage.getItem("image")) || 2;
+window.addEventListener("load", function () {
+  changeIMG();
+});
+
+right_arrow.addEventListener("click", function () {
+  if (image < 3) {
+    image++;
+  } else {
+    image = 1;
+  }
+  changeIMG();
+  window.localStorage.setItem("image", image);
+});
+
+left_arrow.addEventListener("click", function () {
+  if (image > 1) {
+    image--;
+  } else {
+    image = 3;
+  }
+  changeIMG();
+  window.localStorage.setItem("image", image);
+});
+
+function changeIMG() {
+  landing.style.backgroundImage = `url('images/landing${image}.jpg')`;
+  bullet.forEach((item) => item.classList.remove("active"));
+  bullet[image - 1].classList.add("active");
+}
+
+
