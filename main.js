b480008a49ba2a90e7fc100bb5c5f5fef245f308
Abood3dd <abood3dd4@gmail.com>
2024-01-19 16:28:05 +0300

    editing the tools menu

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 const tools = document.getElementById("tools");
 const toolsContianer = document.getElementById("tools-container");
-
+const main = document.querySelector("section");
+let tools_condition = false;
 tools.addEventListener("click", function () {
   if (toolsContianer.classList.contains("shown")) {
     toolsContianer.classList.remove("shown");
+    tools_condition = false;
   } else {
     toolsContianer.classList.add("shown");
+    tools_condition = true;
   }
 });
-
+main.addEventListener("click", function () {
+  if (tools_condition) {
+    toolsContianer.classList.remove("shown");
+  }
+});
 let btn = document.querySelector(".up");
 window.onscroll = function () {
   if (window.scrollY >= 600) {
